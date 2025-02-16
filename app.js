527a4fd2ffa31ca04d1dc788c30ae9a4e1cef2e4
hlwuser <newu46312@gmail.com>
2025-02-16 14:04:14 +0530

    added a route

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.get("/user", (req, res) => {
+  res.send({ username: "hlwy=user" });
 });
 
 app.listen(port, () => {
